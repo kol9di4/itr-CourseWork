637d3cbb82ae5fddccaff0f3eca1447473a3c391
kol9di4 <kol9di4@gmail.com>
2024-05-29 11:04:08 +0300

    delete buttons for collections and items(not full)

--- public/js/delete-helper.js
+++ public/js/delete-helper.js
@@ -9,10 +9,12 @@ $(function(){
     });
     $('body').on('click','a.item-delete',function(e){
         e.preventDefault();
+        splittedUrl = window.location.pathname.split( '/' );
+        urlForRedirect = window.location.origin+'/'+splittedUrl[1]+'/'+splittedUrl[2];
         url = $(this).attr('href');
         $.post(window.location.origin+url,
             function(response) {
-                window.location.href = window.location.origin;
-            });
+                window.location.href = urlForRedirect;
+        });
     });
 })
